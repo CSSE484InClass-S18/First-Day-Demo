9dcc6dba995986aacacd1dddc9ebcb93b7484941
David Fisher <fisherds@rose-hulman.edu>
2018-03-05 12:09:39 -0500

    Touch events

--- First Day Demo/TouchEventsViewController.swift
+++ First Day Demo/TouchEventsViewController.swift
@@ -10,26 +10,34 @@ import UIKit
 
 class TouchEventsViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var logoImageView: UIImageView!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else {
+      return
     }
-    
+    let position = touch.location(in: view)
+    print("Began")
 
-    /*
-    // MARK: - Navigation
+    UIView.beginAnimations(nil, context: nil)
+    logoImageView.center = position
+    UIView.commitAnimations()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+  }
+
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else {
+      return
     }
-    */
+    let position = touch.location(in: view)
+    print("Moved")
+    logoImageView.center = position
+  }
 
 }
